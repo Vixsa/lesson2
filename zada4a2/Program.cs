51608c1e076828a1dc1792adccbafeccacf0a1b7
Vixsa <victoriagiter@gmail.com>
2023-02-22 13:57:21 +0200

    вторая задача

---
+++ zada4a2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length < 2)
+{
+ Console.WriteLine("третьей цифры нет");
+ return;}
+
+Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+
